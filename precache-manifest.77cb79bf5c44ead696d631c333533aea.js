775c2f68bcf8f63a9f1ae715b30334c869c6fcc3
Nouman Aamir <noumanaamir@zain-bizops.home>
2019-12-17 01:37:28 +0500

    Updates

---
+++ precache-manifest.77cb79bf5c44ead696d631c333533aea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20db589b83d255b29a17058ed9da881",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "384cf2db63443244ffe6",
+    "url": "/weather-app/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "aa21745da3001244ee1f",
+    "url": "/weather-app/static/css/main.c4aedefb.chunk.css"
+  },
+  {
+    "revision": "384cf2db63443244ffe6",
+    "url": "/weather-app/static/js/2.78ada826.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/weather-app/static/js/2.78ada826.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa21745da3001244ee1f",
+    "url": "/weather-app/static/js/main.2dce39d7.chunk.js"
+  },
+  {
+    "revision": "5b02ce33b4b8525c3b4d",
+    "url": "/weather-app/static/js/runtime-main.fa4712b1.js"
+  }
+]);
